ceefa1317fbaeca57e5673b5e753aa87689863f3
TonyTan <2906375081@qq.com>
2026-06-23 17:08:10 +0800

    添加角色人物通用文件

---
+++ src/character.cpp
@@ -0,0 +1,56 @@
+#include "character.h"
+#include <utility>
+#include <algorithm>
+
+Character::Character(std::string name)
+    : name_(std::move(name)),
+      x_(0.0F),
+      y_(0.0F),
+      verticalVelocity_(0.0F),
+      isGrounded_(true)
+{
+} //输入角色名字就创建了，里面包含基础属性，具体数值不在这里调整，这只是通用配置。
+
+void Character::moveRight(float deltaTime) {
+    x_ += moveSpeed_ * deltaTime;
+} //右移
+
+void Character::moveLeft(float deltaTime) {
+    x_ -= moveSpeed_ * deltaTime;
+} //左移
+//deltaTime 是上一帧到当前帧经过了多少秒。
+
+void Character::jump() {
+    if (!isGrounded_) return;
+
+    verticalVelocity_ = jumpInitialVelocity_;
+    isGrounded_ = false;
+
+} //跳跃
+
+void Character::takeDamage(float damage) {
+    if (damage <= 0.0F) return;
+
+    health_ = std::max(0.0F, health_ - damage);
+} //受击伤害
+
+void Character::heal(float value) {
+    if (value <= 0.0F) return;
+
+    health_ = std::min(maxHealth_, health_ + value);
+} //回血量
+    
+bool Character::isAlive() const {
+    return health_ > 0.0F;
+} //检测存活
+
+void Character::update(float deltaTime, float gravity) {
+    if (!isAlive) return;
+
+    if (!isGrounded_) {
+        verticalVelocity_ -= gravity * deltaTime;
+        y_ += verticalVelocity_ * deltaTime;
+    } //跳跃更新函数，当不在地面上时，竖直方向上的速度=加速度对时间的积分，竖直高度=速度对时间的积分
+
+    
+} //每一帧更新角色函数, 很重要！！！！所有角色逻辑都在这里面
---
+++ src/character.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+
+class Character {
+public:
+    explicit Character(std::string name);//创建人物要填写名字
+
+    void moveRight(float deltaTime); //右移
+    void moveLeft(float deltaTime); //左移
+    //deltaTime 是上一帧到当前帧经过了多少秒。
+    virtual void jump(); //跳跃
+    void takeDamage(float damage); //受击伤害
+    void heal(float value); //回血量
+    void update(float deltaTime, float gravity); //每一帧更新角色函数
+    
+    bool isAlive() const; //检测存活
+
+protected:
+    std::string name_; //角色名字
+
+    float x_; //角色x轴位置信息
+    float y_; //角色y轴位置信息
+    float moveSpeed_; //角色移动速度
+    float physicalDefense_; //角色物理防御
+    float magicalDefense_; //角色魔法防御
+    float health_; //角色当前血量
+    float maxHealth_; //角色最大血量
+    float attackDamage_; //角色攻击伤害
+    float attacksPerSecond_; //角色每秒攻击速度
+    float shield_; //角色护盾值
+
+    //我觉得可以给不同场景设置不同的重力，影响跳跃高度和下落速度
+    float jumpInitialVelocity_; //角色跳跃初速度
+    float verticalVelocity_; //角色当前的垂直速度
+    float gravityScale_; //角色收到的重力加速度
+
+    bool isGrounded_; //检测角色是否下落到实体上
+
+    
+
+private:
+
+
+};
