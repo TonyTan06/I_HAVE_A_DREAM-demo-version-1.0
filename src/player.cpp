56328a8fd215766d20224e0a5994901ab3b85971
TonyTan06 <97751606+TonyTan06@users.noreply.github.com>
2026-06-24 12:52:17 +0800

    新增主角金币和经验 (#8)
    * 添加角色人物通用文件
    
    * 新增主角代码
    
    * 人物经验升级
    
    * 新增主角经验，金币系统
    
    * 新增经验，金币系统（改）
    
    * 新增主角金币和经验
    
    * 修改金币和经验系统

--- src/player.cpp
+++ src/player.cpp
@@ -1,11 +1,16 @@
 #include "player.h"
 #include <utility>
+#include <iostream>
+#include <cmath>
 
 Player::Player(std::string name)
     : Character(std::move(name)),
       jumpCount_(0),
       maxJumpCount_(2),
-      gravityScale_(1.0F){
+      gravityScale_(1.0F),
+      experience_(1),
+      level_(1),
+      gold_(0){
       }
 // 创建了主角，并且可以二连跳
 
@@ -31,10 +36,61 @@ void Player::update(float deltaTime, float gravity) {
     if (isGrounded_) jumpCount_ = 0;
 } //主角的状态更新，主角独有的加在这里面
 
+void Player::addGold(int value) {
+    if (value <= 0) return;
+
+    gold_ += value;
+}
+
+void Player::addExperience(int value) {
+    if (value <= 0) return;
+
+    experience_ += value;
+
+    while (experience_ >= getExperienceThreshold()) {
+        levelUp();
+    }
+}
+
+void Player::levelUp() {
+    experience_ -= getExperienceThreshold();
+    ++level_;
+    /*
+    maxHealth_ += ;
+    health_ = maxHealth_;
+    attackDamage_ += ;
+    attacksPerSecond_ += ;
+    physicalDefense_ += ;
+    magicalDefense_ += ;
+    */
+
+    std::cout << "LEVEL UP!" << std::endl;
+}
+
+void Player::attack() {
+    std::cout << "Attack!!!" << std::endl;
+}
+
+int Player::getExperienceThreshold() const{
+    return static_cast<int>(10.0F * (level_ + level_ * std::log(level_)));
+}
+
 int Player::getJumpCount() const {
     return jumpCount_;
-}
+} //获取当前跳跃次数
 
 int Player::getMaxJumpCount() const {
     return maxJumpCount_;
-}
+} //获取最大跳跃次数
+
+int Player::getGold() const {
+    return gold_;
+} //获取当前金币数量
+
+int Player::getExperience() const {
+    return experience_;
+} //显示当前经验值
+
+int Player::getLevel() const {
+    return level_;
+} //显示当前等级
