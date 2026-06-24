2629ab9bb583bb2b5ec141a975dc70397b1db8cd
TonyTan <2906375081@qq.com>
2026-06-24 11:28:52 +0800

    新增主角经验，金币系统

--- src/player.h
+++ src/player.h
@@ -11,15 +11,25 @@ public:
     void moveLeft(float deltaTime); //左移
     void jump() override; //跳跃
     void update(float deltaTime, float gravity) override; //主角状态更新
+    void addGold(int value); //获得金币
+    void addExperience(int value); //获得经验
+    void levelUp(); //主角升级
+    void attack() override;
 
     int getJumpCount() const; //跳跃次数（可以实现二连跳）
     int getMaxJumpCount() const; //最大跳跃次数
-
-
+    int getGold() const; 
+    int getExperience() const;
+    int getLevel() const;
+    int getExperienceThreshold() const; //升级需要的经验
 
 private:
     int jumpCount_;
     int maxJumpCount_;
     float gravityScale_; //单位是g
+
+    int experience_; //主角经验值
+    int level_; //主角等级
+    int gold_; //主角金币
 
 };
