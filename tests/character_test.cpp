2e203fe761e0b58213ae8e52f948036bb811cbda
Terry <174591155+superkakater@users.noreply.github.com>
2026-06-30 19:28:11 -0400

    ci/add_new_workflow: adding base tests for the current src files

---
+++ tests/character_test.cpp
@@ -0,0 +1,19 @@
+#include "character.h"
+
+#include <gtest/gtest.h>
+
+TEST(CharacterTest, StartsAliveAndUsesDefaultGravityScale) {
+    Character character("Test Character");
+
+    EXPECT_TRUE(character.isAlive());
+    EXPECT_FLOAT_EQ(character.getGravityScale(), 1.0F);
+}
+
+TEST(CharacterTest, RejectsNegativeGravityScale) {
+    Character character("Test Character");
+
+    character.setGravityScale(2.0F);
+    character.setGravityScale(-1.0F);
+
+    EXPECT_FLOAT_EQ(character.getGravityScale(), 2.0F);
+}
